825fe1d6f0791e70f359cd96aa0fc4e899ce35e2
Hamed <infinitive66@gmail.com>
2024-12-06 12:16:40 +0330

    Add Last Edit

--- yolo/yolo.py
+++
@@ -1,7 +0,0 @@
-train: D:/ProjectPython/ReadCaptcha/labeling_project/image_labeler/static/images
-val: D:/ProjectPython/ReadCaptcha/labeling_project/image_labeler/static/images
-nc: 10  # تعداد کلاس‌ها
-
-names: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  # لیبل کلاس‌ها
-        
-    
